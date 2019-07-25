8cf6a09272bf44687875f46d53505b8748aa7709
carmenpmoreno <carmenp.moreno@gmail.com>
2019-07-25 14:36:14 +0200

    fetch adalab users and create a mock of users to work

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import fetchAdalabUsers from '../services/getAdalabUsers';
+// import fetchAdalabUsers from '../services/getAdalabUsers';
+import mockAdalabUsers from '../services/getAdalabUsers';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      AdalabUsersdata: [],
+      adalabUsersdata: {},
       fetchAdalabUsersOk: false,
       inputValue: '',
     };
@@ -19,22 +20,29 @@ class App extends React.Component {
   }
 
   getAdalabUsers() {
-  fetchAdalabUsers()
-    .then(data => {
-      this.setState({
-        AdalabUsersdata: [data],
-        fetchAdalabUsersOk: true,
-      });
+    // fetchAdalabUsers()
+    //   .then(adalabUsers => {
+    this.setState({
+      adalabUsersdata: { mockAdalabUsers },
+      fetchAdalabUsersOk: true,
     });
   }
 
   render() {
-        return(
-      <div className = "App" >
-            <h1>GitHub finder of people on Adalab</h1>
+    const adalabUsers = this.state.adalabUsersdata.mockAdalabUsers;
+    console.log(adalabUsers);
+    return (
+      <div className="App" >
+        <h1>GitHub finder of people on Adalab</h1>
+        <ul>
+          {this.state.fetchAdalabUsersOk === true
+            ? (adalabUsers.map(adalabUser => <li key={adalabUser.id}>{adalabUser.login}</li>))
+            : (<p>Loading ...</p>)
+          }
+        </ul>
       </div>
     );
-}
+  }
 }
 
 export default App;
