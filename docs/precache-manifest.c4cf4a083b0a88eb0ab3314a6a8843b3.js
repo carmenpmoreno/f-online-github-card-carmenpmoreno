714dd51ac5ab7c91c41a366f5bd96f8f9a66c54e
carmenpmoreno <carmenp.moreno@gmail.com>
2019-07-29 15:39:26 +0200

    build docs

---
+++ docs/precache-manifest.c4cf4a083b0a88eb0ab3314a6a8843b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54e69fdfa87622e9684034f3267fb5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd29ca2a16efb792c571",
+    "url": "/static/css/main.70a090ea.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "dd29ca2a16efb792c571",
+    "url": "/static/js/main.0c5fefaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
